9fb356c647fb1a62dbd788462262469b9324ec5d
Solha Park <solhapark@milan2.cm.cluster>
2026-03-07 02:35:57 -0500

    Update run_query_example.sh

--- scripts/run_query_example.sh
+++ scripts/run_query_example.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=run_query_example
 #SBATCH --nodes=1
-#SBATCH --partition=short-96core
+#SBATCH --partition=short-40core
 #SBATCH --mem=4G
 #SBATCH --cpus-per-task=4
 #SBATCH --time=4:00:00
-#SBATCH --output=/dev/null
-#SBATCH --error=/dev/null
+#SBATCH --output=logs/run_query_example.out
+#SBATCH --error=logs/run_query_example.err
+
+set -euo pipefail
 
 # Activate conda env
 if command -v module &>/dev/null; then
   module load anaconda/3
+  module load gcc/12.3.0
 fi
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate /gpfs/home/solhapark/envs/infinigram
